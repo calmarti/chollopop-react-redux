83afced7883dfc4ef27e7e9e5bd2b751ebe6a00b
davidjj76 <davidjj76@gmail.com>
2021-11-03 23:10:45 +0100

    UseMutation for logout

--- src/components/auth/AuthButton/AuthButton.js
+++ src/components/auth/AuthButton/AuthButton.js
@@ -4,10 +4,13 @@ import T from 'prop-types';
 import { ConfirmationButton } from '../../common';
 import { AuthConsumer } from '../context';
 import { logout } from '../service';
+import useMutation from '../../../hooks/useMutation';
 
 const AuthButton = ({ handleLogout, isLogged }) => {
+  const mutation = useMutation(logout);
+
   const handleLogoutConfirm = async () => {
-    await logout();
+    await mutation.execute();
     handleLogout();
   };
 
