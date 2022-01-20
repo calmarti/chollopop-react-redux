e0c4c7f3138b8ff89dbe4d530f8522e5db556992
Ivan Martinez <ivmartinezc@gmail.com>
2022-01-20 12:35:35 +0100

    test unitarios: selector y primera parte de tests a acciones asincronas

--- src/store/actions.test.js
+++ src/store/actions.test.js
@@ -1,4 +1,4 @@
-import { authLoginRequest, loadAdvertsSuccess } from "./actions";
+import { authLogin, authLoginRequest, loadAdvertsSuccess } from "./actions";
 import { AUTH_LOGIN_REQUEST, LOAD_ADVERTS_SUCCESS } from "./types";
 
 describe("testing authLoginRequest action creator", () => {
@@ -8,6 +8,50 @@ describe("testing authLoginRequest action creator", () => {
     expect(result).toEqual(expected);
   });
 });
+
+//TODO: el test pasa pero hay que arreglar la recepción de parámetros en authLogin (actions.js) y probar si funciona toda la app
+describe("testing authLogin action creator", () => {
+  const remember = false;
+  const credentials = {
+    mockedEmail: "mockedEmail",
+    mockedPassword: "mockedPassword",
+  };
+  const location = { state: "" };
+  const action = authLogin({ remember, ...credentials }, location);
+
+  describe("when login api resolves", () => {
+    const dispatch = jest.fn();
+    const getState = () => {};
+    const api = { auth: { login: jest.fn().mockResolvedValue() } };
+    const history = { replace: jest.fn() };
+    test("should dispatch action AUTH_LOGIN_REQUEST", () => {
+      action(dispatch, getState, { api, history });
+      expect(dispatch).toHaveBeenCalledWith({ type: "AUTH_LOGIN_REQUEST" });
+    });
+    test("should execute api.auth.login", () => {
+      // const {remember, mockedEmail, mockedPassword} = credentials;
+      action(dispatch, getState, { api, history });
+      expect(api.auth.login).toHaveBeenCalledWith(remember, credentials);
+      //TODO: arreglar: expect(api.auth.login(remember, credentials)).resolves();
+    });
+
+    //TODO: usar otra de las formas para promesas del módulo de TDD (distinta a async-await)
+    test("should dispatch action AUTH_LOGIN_SUCCESS", async () => {
+      await action(dispatch, getState, { api, history });
+      expect(dispatch).toHaveBeenNthCalledWith(2, { type: "AUTH_LOGIN_SUCCESS" });
+    });
+
+//test("should redirect to "/", ()=> {
+
+//})
+
+  // describe("when login api rejects", () => {
+
+
+  });
+});
+
+
 
 describe("testing loadAdvertsSuccess action creator", () => {
   test("should return an object with a type and a payload", () => {
--- src/store/selectors.test.js
+++ src/store/selectors.test.js
@@ -1,4 +1,32 @@
+import { loadAdvertSelector } from "./selectors";
 
 describe("testing loadAdvertSelector", () => {
+  test("if advertId matches an id in state.adverts.data then it should return that advert", () => {
+    const advertId = "advertId";
+    const state = {
+      auth: true,
+      ui: { isLoading: false, error: null },
+      adverts: {
+        isLoaded: true,
+        data: [{ id: "otherId" }, { id: "advertId" }, { id: "otherId" }],
+      },
+    };
+    const expected = { id: "advertId" };
+    expect(loadAdvertSelector(state, advertId)).toMatchObject(expected);
+  });
 
-})
+  test("if advertId does not match an id in state.adverts.data then it should return undefined", () => {
+    const advertId = "advertId";
+    const state = {
+      auth: true,
+      ui: { isLoading: false, error: null },
+      adverts: {
+        isLoaded: true,
+        data: [{ id: "otherId" }, { id: "otherId" }, { id: "otherId" }],
+      },
+    };
+    expect(loadAdvertSelector(state, advertId)).toBeUndefined();
+  });
+});
+
+//TODO: test de un thunk
